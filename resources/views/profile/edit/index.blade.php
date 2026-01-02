eaef9e63bc1b50261eaaadccfc82ab4043edd0d1
Wisnutomo Adji Nugroho <wisnutomo2308@gmail.com>
2026-01-03 02:17:02 +0700

    update gender radio button

--- resources/views/profile/edit/index.blade.php
+++ resources/views/profile/edit/index.blade.php
@@ -27,17 +27,17 @@
             {{-- gender --}}
             <div class="w-[85%] flex gap-20">
                 <label for="gender" class="w-full font-roboto text-[16px]">Gender</label>
-                <div class="w-full flex items-center justify-center p-1">
+                <div class="w-full flex items-center justify-center">
 
 
-                    <div class="w-full h-8 flex justify-center items-center rounded-2xl overflow-hidden">
+                    <div class="w-full h-8 flex justify-center items-center rounded-md overflow-hidden">
                         
                     {{-- male radio button --}}
                         <div class="w-1/2 flex">
                             <input type="radio" name="gender" id="gender_male" value="Male" class="hidden peer" checked>
                             <label for="gender_male" class="w-full bg-black text-white
                             peer-checked:bg-gray-500/60  
-                            text-center font-roboto py-2 px-3.5 cursor-pointer 
+                            text-center font-roboto py-2 px-3.5 cursor-pointer
                             transition-all duration-500">Male</label>
                         </div>
             
